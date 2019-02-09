b0ae22a76b407cd3262f613c3b585d9fbc70528e
mfrthompson <47467595+mfrthompson@users.noreply.github.com>
2019-02-09 10:41:20 -0600

    Uploaded my mad-lib project

---
+++ madlib.py
@@ -0,0 +1,12 @@
+
+place_1 = input("Enter a place: ")
+noun_1 = input("Enter a noun: ")
+verb_1 = input("Enter a verb: ")
+noun_2 = input("Enter another noun: ")
+yell_1 = input("Enter something you would yell: ")
+
+print("There once was a man from " + place_1 + ".")
+print("With a " + noun_1 + " so long he could " + verb_1 + " with it.")
+print("The man from " + place_1 + " would keep all of his money in a " + noun_2 + ".")
+print("Sometimes the man would forget where he put the " + noun_2 + " and he would yell " + yell_1 + "!!")
+
