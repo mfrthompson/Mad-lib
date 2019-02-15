22a7a94c4f3727fa8f09557cedeffcc6e28352f5
mfrthompson <47467595+mfrthompson@users.noreply.github.com>
2019-02-14 20:37:08 -0600

    Nested Function
    Added nested function that enables the program to continue past first set of rolls if the user wishes.

--- Dice/dice.py
+++ Dice/dice.py
@@ -3,19 +3,26 @@ import sys
 
 
 def dice_roll():
-    try:
-        side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
-    except ValueError:
-        print("Numbers only please")
-        sys.exit()
-    try:
-        roll_amt = int(input("How many times would you like the dice to roll?"))
-    except ValueError:
-        print("Numbers only please")
-        sys.exit()
+    def user():
+        try:
+            side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
+        except ValueError:
+            print("Numbers only please")
+            user()
+        try:
+            roll_amt = int(input("How many times would you like the dice to roll?"))
+        except ValueError:
+            print("Numbers only please")
+            user()
 
-    for _ in range(roll_amt):
-        print(random.randint(1, side_dice))
+        for _ in range(roll_amt):
+            print(random.randint(1, side_dice))
+            cont = input("Would you like to roll again? Y/N")
+            if cont == "Y" or "y":
+                user()
+            elif cont == "N" or "n":
+                sys.exit()
+    user()
 
 
 dice_roll()
