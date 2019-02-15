55f53de4b24af1f371d0bbfbec6b34eabc463de1
mfrthompson <47467595+mfrthompson@users.noreply.github.com>
2019-02-14 23:11:03 -0600

    more adjustments
    added in return.

--- Dice/dice.py
+++ Dice/dice.py
@@ -2,28 +2,35 @@ import random
 import sys
 
 
-def dice_roll():
-    def user():
-        try:
-            side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
-        except ValueError:
-            print("Numbers only please")
-            user()
-        try:
-            roll_amt = int(input("How many times would you like the dice to roll?"))
-        except ValueError:
-            print("Numbers only please")
-            user()
-
-        for _ in range(roll_amt):
-            print(random.randint(1, side_dice))
-            cont = input("Would you like to roll again? Y/N")
-            if cont == "Y" or "y":
-                user()
-            elif cont == "N" or "n":
-                sys.exit()
-    user()
+def size():
+    try:
+        side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
+        return side_dice
+    except ValueError:
+        print("Numbers only please")
+        size()
 
 
-dice_roll()
+def amount():
+    try:
+        roll_amt = int(input("How many times would you like to roll the dice?"))
+        return roll_amt
+    except ValueError:
+        print("Numbers only please")
+        amount()
+
+
+def main():
+    side_dice = size()
+    roll_amt = amount()
+    for _ in range(roll_amt):
+        print(random.randint(1, side_dice))
+    cont = input("Would you like to roll again? Y/N")
+    if cont.lower() == "y":
+        main()
+    elif cont.lower() == "n":
+        sys.exit()
+
+
+main()
 
