14463e56ec4701b4630e395b35692a4fa02f2418
mfrthompson <47467595+mfrthompson@users.noreply.github.com>
2019-02-14 20:01:32 -0600

    Error Handling
    added try/except error handling for the user input.

--- Dice/dice.py
+++ Dice/dice.py
@@ -5,10 +5,15 @@ import sys
 def dice_roll():
     try:
         side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
+    except ValueError:
+        print("Numbers only please")
+        sys.exit()
+    try:
         roll_amt = int(input("How many times would you like the dice to roll?"))
     except ValueError:
         print("Numbers only please")
         sys.exit()
+
     for _ in range(roll_amt):
         print(random.randint(1, side_dice))
 
