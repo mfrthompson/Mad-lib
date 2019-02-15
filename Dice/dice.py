688dd20dedc6ba897ee7b84b2349e33bbd18958f
mfrthompson <47467595+mfrthompson@users.noreply.github.com>
2019-02-14 19:58:40 -0600

    Version change
    I cleaned up the code from using if/elif statements to a simple for loop setup that cut the amount of code needed in half.

--- Dice/dice.py
+++ Dice/dice.py
@@ -1,40 +1,17 @@
+import random
+import sys
 
-from random import *
+
+def dice_roll():
+    try:
+        side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
+        roll_amt = int(input("How many times would you like the dice to roll?"))
+    except ValueError:
+        print("Numbers only please")
+        sys.exit()
+    for _ in range(roll_amt):
+        print(random.randint(1, side_dice))
 
 
-def dice():
-    num_dice = input("Hello! Would you like to roll 1 or 2 dice?")
-    side_dice = input("Ok, would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?")
-    if(side_dice == "4" and num_dice == "1"):
-        print(randint(1, 6))
-    elif(side_dice == "4" and num_dice == "2"):
-        print(randint(1, 6))
-        print(randint(1, 6))
-    elif(side_dice == "6" and num_dice == "1"):
-        print(randint(1, 6))
-    elif(side_dice == "6" and num_dice == "2"):
-        print(randint(1, 6))
-        print(randint(1, 6))
-    elif(side_dice == "8" and num_dice == "1"):
-        print(randint(1, 6))
-    elif(side_dice == "8" and num_dice == "2"):
-        print(randint(1, 6))
-        print(randint(1, 6))
-    elif(side_dice == "10" and num_dice == "1"):
-        print(randint(1, 6))
-    elif(side_dice == "10" and num_dice == "2"):
-        print(randint(1, 6))
-        print(randint(1, 6))
-    elif(side_dice == "12" and num_dice == "1"):
-        print(randint(1, 6))
-    elif(side_dice == "12" and num_dice == "2"):
-        print(randint(1, 6))
-        print(randint(1, 6))
-    elif(side_dice == "20" and num_dice == "1"):
-        print(randint(1, 6))
-    elif(side_dice == "20" and num_dice == "2"):
-        print(randint(1, 6))
-        print(randint(1, 6))
-
-dice()
+dice_roll()
 
