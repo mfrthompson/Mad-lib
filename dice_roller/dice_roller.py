dcf6793b155cba852f924d25b8e33c43fbe903b2
mfrthompson <47467595+mfrthompson@users.noreply.github.com>
2019-02-17 08:54:21 -0600

    Create dice_roller.py

---
+++ dice_roller/dice_roller.py
@@ -0,0 +1,36 @@
+import random
+
+
+def size():
+    try:
+        side_dice = int(input("Would you like to roll the 4, 6, 8, 10, 12,or 20 sided dice?"))
+        return side_dice
+    except ValueError:
+        print("Numbers only please")
+        size()
+
+
+def amount():
+    try:
+        roll_amt = int(input("How many times would you like to roll the dice?"))
+        return roll_amt
+    except ValueError:
+        print("Numbers only please")
+        amount()    
+    
+    
+def again():
+    cont = input("Would you like to roll again? Y/N")
+    if cont.lower() == "y":
+        main()
+
+
+def main():
+    side_dice = size()
+    roll_amt = amount()
+    for _ in range(roll_amt):
+        print(random.randint(1, side_dice))
+    again()
+
+
+main()
